891bb706ec06c62359a77a7e60fc3eba19064d4c
Güray Gül <guraygul59@gmail.com>
2023-10-09 21:59:50 +0300

    Options menu added

---
+++ PomodoroApp/OptionProperties.swift
@@ -0,0 +1,8 @@
+//
+//  OptionProperties.swift
+//  PomodoroApp
+//
+//  Created by Güray Gül on 8.10.2023.
+//
+
+import Foundation
---
+++ PomodoroApp/OptionView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionView.swift
+//  PomodoroApp
+//
+//  Created by Güray Gül on 8.10.2023.
+//
+
+import SwiftUI
+
+struct OptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionView()
+}
