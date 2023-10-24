ee6c6565dfac324bf1dca03a210f9ece472c6a5c
Güray Gül <guraygul59@gmail.com>
2023-10-24 19:32:51 +0300

    Properities and UI has been chanhed

---
+++ PomodoroApp/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  PomodoroApp
+//
+//  Created by Güray Gül on 24.10.2023.
+//
+
+import Foundation
--- PomodoroApp/OptionView.swift
+++ PomodoroApp/OptionView.swift
@@ -2,41 +2,17 @@
 //  OptionView.swift
 //  PomodoroApp
 //
-//  Created by Güray Gül on 8.10.2023.
+//  Created by Güray Gül on 23.10.2023.
 //
 
 import SwiftUI
 
-import SwiftUI
-
 struct OptionView: View {
-    @Binding var timeRemaining: CGFloat // Ana ekrandaki zamanlayıcıyı değiştirmek için kullanacağımız bağlı değişken
-    
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Zamanlayıcı Ayarları")) {
-                    Stepper(value: $timeRemaining, in: 1...200000, step: 60) {
-                        Text("Süre: \(Int(timeRemaining) / 60) dakika")
-                    }
-                }
-            }
-            .navigationTitle("Ayarlar")
-            .toolbar {
-                Button("Kaydet") {
-                    // Ayarları kaydetmek için bir şeyler yapabilirsiniz (örneğin, UserDefaults kullanarak)
-                    // Ayrıca, kullanıcının seçtiği süreyi kaydetmelisiniz.
-                    UserDefaults.standard.set(Int(timeRemaining), forKey: "selectedTime")
-                        
-                        // OptionView'i kapatın
-                   // presentationMode.wrappedValue.dismiss()
-                }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
-//#Preview {
-//    OptionView()
-//}
+#Preview {
+    OptionView()
+}
